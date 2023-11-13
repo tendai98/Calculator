3598caea6e426f44e1d2aa570272ae778e5432a6
Tendai Bhebhe <tendaibhebhe2018@gmail.com>
2023-11-13 12:48:37 +0000

    add functions

--- app/src/main/java/com/tendai/calculator/MainActivity.kt
+++ app/src/main/java/com/tendai/calculator/MainActivity.kt
@@ -3,15 +3,26 @@ package com.tendai.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var inputTextView: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        inputTextView = findViewById(R.id.text_view_input)
+
     }
 
     fun onDigit(view: View){
-        Toast.makeText(applicationContext, "Button Clicked", Toast.LENGTH_SHORT).show()
+        inputTextView?.append("1")
+    }
+
+    fun onClear(view: View){
+
     }
 }
